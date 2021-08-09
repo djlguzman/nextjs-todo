495920dc2ff456878dddeee77b6659ccf9f1d0d9
djlguzman <82568705+djlguzman@users.noreply.github.com>
2021-08-09 13:01:40 +0800

    changed transition module from react-transition-group to framer-motion

--- components/todo-page/todo-item.js
+++ components/todo-page/todo-item.js
@@ -1,13 +1,25 @@
 import React, { useRef, useState, useEffect } from "react";
-import styled from "styled-components";
 import { BsFillTrashFill as DeleteIcon } from "react-icons/bs";
 import { FaEdit as EditIcon } from "react-icons/fa";
 import classes from "./todo-item.module.css";
+import { motion, usePresence } from "framer-motion";
 
 const TodoItem = ({ todoText, onRemove, onUpdate }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [fieldValue, setFieldValue] = useState("");
   const inputRef = useRef();
+  const [isPresent, safeToRemove] = usePresence();
+
+  const animations = {
+    layout: true,
+    initial: "out",
+    animate: isPresent ? "in" : "out",
+    variants: {
+      in: { scaleY: 1, opacity: 1 },
+      out: { scaleY: 0, opacity: 0 },
+    },
+    onAnimationComplete: () => !isPresent && safeToRemove(),
+  };
 
   useEffect(() => {
     if (isEditing) {
@@ -29,7 +41,7 @@ const TodoItem = ({ todoText, onRemove, onUpdate }) => {
   };
 
   return (
-    <li className={classes.item}>
+    <motion.li className={classes.item} {...animations}>
       {isEditing && (
         <input
           ref={inputRef}
@@ -44,47 +56,8 @@ const TodoItem = ({ todoText, onRemove, onUpdate }) => {
           <DeleteIcon className={classes.icons} onClick={onRemove} />
         )}
       </div>
-    </li>
+    </motion.li>
   );
 };
 
 export default TodoItem;
-
-// const Item = styled.li`
-//   word-break: break-all;
-//   list-style: none;
-//   text-align: start;
-//   background-image: linear-gradient(
-//     135deg,
-//     hsla(0, 0%, 100%, 0.6),
-//     hsla(0, 0%, 100%, 0.1)
-//   );
-//   color: #18171d;
-//   padding: 10px;
-//   margin: 5px;
-//   border-radius: 6px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-// `;
-
-// const EditField = styled.input``;
-
-// const IconsContainer = styled.div`
-//   display: flex;
-//   align-items: center;
-// `;
-
-// const EditIcon = styled(FaEdit)`
-//   font-size: 1.5rem;
-//   cursor: pointer;
-//   color: #e8e4c9;
-//   margin-left: 10px;
-// `;
-
-// const DeleteIcon = styled(BsFillTrashFill)`
-//   font-size: 1.5rem;
-//   cursor: pointer;
-//   color: #e8e4c9;
-//   margin-left: 10px;
-// `;
--- components/todo-page/todo-list.js
+++ components/todo-page/todo-list.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { AnimatePresence } from "framer-motion";
 import classes from "./todo-list.module.css";
 
 import TodoContext from "../../store/todo-context";
@@ -9,40 +9,23 @@ import TodoItem from "./todo-item";
 const TodoList = () => {
   const todoCtx = useContext(TodoContext);
   return (
-    <TransitionGroup component="ul" className={classes.list}>
-      {todoCtx.todos.map(({ id, text }) => {
-        return (
-          <CSSTransition key={id} timeout={250} classNames="transition">
+    <ul className={classes.list}>
+      <AnimatePresence>
+        {todoCtx.todos.map(({ id, text }) => {
+          return (
             <TodoItem
+              key={id}
               onRemove={() => {
                 todoCtx.removeTodo(id);
               }}
               onUpdate={todoCtx.updateTodo.bind(null, id)}
               todoText={text}
             />
-          </CSSTransition>
-        );
-      })}
-    </TransitionGroup>
+          );
+        })}
+      </AnimatePresence>
+    </ul>
   );
 };
 
 export default TodoList;
-
-{
-  /* <TransitionGroup component={"ul"}>
-      {todoCtx.todos.map(({ id, text }) => {
-        return (
-          <CSSTransition key={id} timeout={250} classNames="transition">
-            <TodoItem
-              onRemove={() => {
-                todoCtx.removeTodo(id);
-              }}
-              onUpdate={todoCtx.updateTodo.bind(null, id)}
-              todoText={text}
-            />
-          </CSSTransition>
-        );
-      })}
-    </TransitionGroup> */
-}
